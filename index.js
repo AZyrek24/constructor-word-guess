ef50944eba3057dcd8bc010c602762ee5c88e523
AZyrek24 <zzeppelin2400@gmail.com>
2018-07-09 15:07:50 -0700

    index.js pickWord function gets word from wordlist.txt

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ var fs = require("fs");
 // Variables
 //======================================================================================
 var guessesRemaining = 9;
+var wordArray = [];
+var pickedWord = "";
 
 // Functions
 //======================================================================================
 function start() {
-  instructions();
+  pickWord();
 }
 function instructions() {
   inquirer.prompt([{
@@ -27,7 +29,20 @@ function instructions() {
     },
   }
   ]).then(function (answers) {
-    console.log(answers.guess);
+    console.log(answers.guess.toUpperCase());
+  });
+}
+function pickWord() {
+  fs.readFile("wordlist.txt", "utf8", function (error, data) {
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
+    //Creates and array of words (from the wordlist.txt data) to select a random word from 
+    wordArray = data.split(",");
+    console.log(wordArray);
+    pickedWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    console.log(pickedWord);
   });
 }
 
