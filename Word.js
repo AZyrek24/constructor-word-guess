58a3de2ca3eadc0e717d8f8108ba87854eff96b7
AZyrek24 <zzeppelin2400@gmail.com>
2018-07-10 13:39:19 -0700

    updated Word.js with letterguess function

--- Word.js
+++ Word.js
@@ -2,28 +2,25 @@ var Letter = require("./Letter.js");
 
 //Word constructor
 var Word = function (randomWord) {
-  this.letterObjectsArray = randomWord.split('').map(function(element){return new Letter(element)});
-  this.wordDisplay = function(letters) {
+  this.letterObjectsArray = randomWord.split('').map(function (element) { return new Letter(element) });
+  this.wordDisplayBuilder = function () {
     var wordtoGuess = "";
     for (var i = 0; i < this.letterObjectsArray.length; i++) {
       wordtoGuess += this.letterObjectsArray[i].charReturn() + " ";
     }
+    console.log(wordtoGuess);
     return wordtoGuess;
   }
-  // this.checkGuess = function (guessedLetter) {
-  //   this.lettersArray.forEach(element => {
-  //     element.checkGuess(guessedLetter);
-  //   });
-  //   console.log(this.lettersArray);
-  // }
+  this.newLetterGuessed = function (guessedLetter) {
+    for (var i = 0; i < this.letterObjectsArray.length; i++) {
+      this.letterObjectsArray[i].checkGuess(guessedLetter);
+    }
+    newWord.wordDisplayBuilder();
+  }
 }
 
 module.exports = Word;
 
-
-var newWord = new Word("HELLO");
-
-newWord.wordDisplay();
 
 
 
